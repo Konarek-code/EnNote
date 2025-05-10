6abb07ebb88aa99328844cff2bd61e11c04c0008
unknown <damian.konarski71@gmail.com>
2025-05-10 16:31:57 +0200

    improved stats screen

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,14 +1,18 @@
 import { Stack } from "expo-router";
 import { StatusBar } from "react-native";
+import { Provider } from "react-redux";
+import { store } from "../../store/store";
 
 const StackLayout = () => {
   return (
     <>
-      <StatusBar barStyle="light-content" backgroundColor="#1A237E" />
-      <Stack>
-        <Stack.Screen name="index" options={{ headerShown: false }} />
-        <Stack.Screen name="screens" options={{ headerShown: false }} />
-      </Stack>
+      <Provider store={store}>
+        <StatusBar barStyle="light-content" backgroundColor="#1A237E" />
+        <Stack>
+          <Stack.Screen name="index" options={{ headerShown: false }} />
+          <Stack.Screen name="screens" options={{ headerShown: false }} />
+        </Stack>
+      </Provider>
     </>
   );
 };
